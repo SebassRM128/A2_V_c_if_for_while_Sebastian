cbf3410a6dc851cf4eca598cca67e88c37b3f9fe
SebassRM128 <a.22308051281303@cbtis128.edu.mx>
2024-09-06 09:35:06 -0600

    Add files via upload
    Adjunte dos archivos

---
+++ miscadenas.py
@@ -0,0 +1,21 @@
+
+
+nombre= "Sebastian"
+print(nombre[0])
+print(nombre)
+
+MisNovias=["S","E","B"]  
+for chicas in MisNovias:
+    print(chicas)
+    print ("otro ejemplo")
+    
+    b = "Sebastian Rojas"
+print(b[2:5])
+print (" -- A MAYUSCULAS --")
+a = "Sebastian Rojas"
+print(a.upper())
+print ("Concatenar")
+a = "Sebastian"
+b = "Rojas"
+c = a + b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,15 @@
+import math
+edad=16   #  guarda en edad
+nombre="Nicolas Medina Zubia"
+print("tu edad es:", edad)
+print("tu nombre es:", nombre) 
+print("-- tipo de variables ---")
+x = str(3)    # x will be '3'
+y = int(3.1416)    # y will be 3
+z = float(7)
+print(x)
+print(y)
+print(z)
+print(" -- Funciones matematicas ----")
+x = math.sqrt(64)
+print(x)
